cf59b730559175135265c73f60de40b19dd794fb
Valery Varaksa <varaksav62@gmail.com>
2024-08-15 16:38:40 +0300

    Bug fixes

---
+++ LamodaB2BSDK/Core/Requests/AuthorizationRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+using LamodaB2BSDK.Core.Attributes;
+using LamodaB2BSDK.Core.Enums;
+
+namespace LamodaB2BSDK.Core.Requests;
+
+public class AuthorizationRequest
+{
+    [JsonPropertyName("client_id")]
+    [QueryParameter("client_id")]
+    public string ClientId { get; set; } = string.Empty;
+    
+    [JsonPropertyName("client_secret")]
+    [QueryParameter("client_secret")]
+    public string ClientSecret { get; set; } = string.Empty;
+    
+    [JsonPropertyName("grant_type")]
+    [QueryParameter("grant_type")]
+    public GrantType GrantType { get; set; }
+    [JsonPropertyName("username")]
+    [QueryParameter("username")]
+    public string? Username { get; set; }
+    
+    [JsonPropertyName("password")]
+    [QueryParameter("password")]
+    public string? Password { get; set; }
+    
+    [JsonPropertyName("redirect_uri")]
+    [QueryParameter("redirect_uri")]
+    public string? RedirectUrl { get; set; }
+}
---
+++ LamodaB2BSDK/Core/Requests/PaginationRequest.cs
@@ -0,0 +1,18 @@
+﻿using LamodaB2BSDK.Core.Attributes;
+
+namespace LamodaB2BSDK.Core.Requests;
+
+public class PaginationRequest
+{
+    /// <summary>
+    /// Count of items on the page. Default = 25
+    /// </summary>
+    [QueryParameter("limit")] public int Limit { get; set; } = 25;
+
+    /// <summary>
+    /// Current page number
+    /// </summary>
+    [QueryParameter("page")] public int Page { get; set; } = 1;
+    
+    
+}
