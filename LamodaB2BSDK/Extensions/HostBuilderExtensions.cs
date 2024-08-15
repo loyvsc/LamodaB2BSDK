2c048d2a898f90abe5e31ee5296857ecc45c6f3a
Valery Varaksa <varaksav62@gmail.com>
2024-08-15 16:56:21 +0300

    Create Host Builder extension

---
+++ LamodaB2BSDK/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using LamodaB2BSDK.Core;
+using LamodaB2BSDK.Core.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LamodaB2BSDK.Extensions;
+
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder AddLamodaB2B(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder.ConfigureServices((_, services) =>
+        {
+            services.AddSingleton<LamodaB2BApplication>();
+            services.AddSingleton<LamodaB2BClient>();
+        });
+    }
+}
