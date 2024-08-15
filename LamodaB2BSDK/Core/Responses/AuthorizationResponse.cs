df798951680c691492b9807eb3a9f4f93caa109b
Valery Varaksa <varaksav62@gmail.com>
2024-08-15 16:40:10 +0300

    Bug fix

---
+++ LamodaB2BSDK/Core/Responses/AuthorizationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LamodaB2BSDK.Core.Responses;
+
+public class AuthorizationResponse
+{
+    [JsonPropertyName("access_token")] public string AccessToken { get; set; } = string.Empty;
+    [JsonPropertyName("expires_in")] public string ExpiresIn { get; set; } = string.Empty;
+    [JsonPropertyName("token_type")] public string TokenType { get; set; } = string.Empty;
+    [JsonPropertyName("scope")] public string Scope { get; set; } = string.Empty;
+}
